0edb11f0a4bf216f2c7c20e1df1ef6ffe2bdd3cc
MaLiHa1321 <malihasultana2001@gmail.com>
2024-03-16 20:16:17 +0600

    banner are modified

--- kcurry-express/src/Component/Navbar/Navbar.jsx
+++ kcurry-express/src/Component/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-slate-400 fixed top-0 w-full">
+        <div className="navbar bg-sky-200 fixed top-0 w-full">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- kcurry-express/src/Pages/Banner/Banner.jsx
+++ kcurry-express/src/Pages/Banner/Banner.jsx
@@ -2,30 +2,35 @@
 
 const Banner = () => {
     return (
+      
         <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+  <div id="slide1" className="carousel-item relative w-full bg-black opacity-70">
+    <img src="https://i.ibb.co/rxC8Q38/beautiful-wedding-flower-arrangement-seats-along-aisle.jpg" className="w-full md:h-[600px] md:object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
+      <a href="#slide4" className="btn btn-circle">❮--</a> 
+      <a href="#slide2" className="btn btn-circle">--❯</a>
+    </div>
+    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" className="btn btn-circle">❮--</a> 
+      <a href="#slide2" className="btn btn-circle">--❯</a>
     </div>
   </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+  <div id="slide2" className="carousel-item relative w-full  bg-black opacity-70">
+    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full md:h-[600px] md:object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+  <div id="slide3" className="carousel-item relative w-full  bg-black opacity-70">
+    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full md:h-[600px] md:object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+  <div id="slide4" className="carousel-item relative w-full  bg-black opacity-70">
+    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full md:h-[600px] md:object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
